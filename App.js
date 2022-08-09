123e396e9f5236f521078659a517a98d22180777
帕斯昂 <passonTomas@linweiqindeMacBook-Pro.local>
2022-08-09 19:31:16 +0800

    feat: couter UI
    2.1

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.bg]}>
+      <View style={{ height: 100 }}>
+        <Text style={count < 5 ? styles.less : styles.greater}>You clicked {count} times</Text>
+      </View>
+      <View style={{ height: 100 }}>
+        <TouchableOpacity style={styles.button} onPress={() => false}>
+          <Text style={styles.buttonText}>Click</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  bg: { flex:1, paddingTop: 150, alignItems: 'center' },
+  less: { fontSize: 25, color: '#4d3398', fontWeight: 'bold' },
+  greater: { fontSize: 25, color: '#f3845c', fontWeight: 'bold' },
+  button: {
+    width: 150,
+    height: 50,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 10,
+    borderRadius: 10,
+    backgroundColor: '#3498db'
   },
+  buttonText: {
+    fontSize: 25,
+    color: '#fff'
+  }
 });
