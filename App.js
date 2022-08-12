ac174eacc61396cc9ab1985b17f3847617b0b3d6
帕斯昂 <passonTomas@linweiqindeMacBook-Pro.local>
2022-08-13 02:13:41 +0800

    feat: 修正缺少 宣告 navigation 變數 會crash 的問題
    3.1

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
