eb9d6015008c76ba907b889e6e64b98870cf16e7
帕斯昂 <passonTomas@linweiqindeMacBook-Pro.local>
2022-08-14 08:53:20 +0800

    feat: update to tab navigatioin example
    3.1.1

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 function HomeScreen({ navigation }) {
   return (
@@ -10,6 +12,38 @@ function HomeScreen({ navigation }) {
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
       />
+    </View>
+  );
+}
+
+function FeedScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Feed Screen</Text>
+    </View>
+  );
+}
+
+function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Notifications Screen</Text>
     </View>
   );
 }
@@ -26,15 +60,41 @@ function DetailsScreen({ navigation }) {
   );
 }
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+      <Tab.Screen options={{
+        headerShown: false,
+        tabBarIcon: () => {
+          return <Ionicons name='home' size={22} />;
+        },
+      }} name="HomeStack" component={HomeStack} />
+      <Tab.Screen options={{
+        tabBarIcon: () => {
+          return <Ionicons name='heart' size={22} />;
+        },
+      }} name="Feed" component={FeedScreen} />
+      <Tab.Screen options={{
+        tabBarIcon: () => {
+          return <Ionicons name='search' size={22} />;
+        },
+      }} name="Notifications" component={NotificationsScreen} />
+    </Tab.Navigator>
     </NavigationContainer>
   );
 }
